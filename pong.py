3d2754004b3891e77f422c519fbc0d361a67a43b
Anton Irvold <40317988+antonvdi@users.noreply.github.com>
2020-01-20 13:47:56 +0100

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,4 +1,4 @@
-import pygame, random
+import pygame, random, time
 
 pygame.init()
 width = 800
@@ -8,6 +8,8 @@ clock = pygame.time.Clock()
 white = (255, 255, 255)
 red = (255, 0, 0)
 blue = (0, 0, 255)
+black = (0, 0, 0)
+font = pygame.font.Font('freesansbold.ttf', 32) 
 
 class player:
 	def __init__(player, name, xpos, ypos, pwidth, pheight, speed, score):
@@ -40,6 +42,7 @@ class ball:
 		ball.xpos += ball.xspeed
 		ball.ypos += ball.yspeed
 		pygame.draw.rect(screen, blue, pygame.Rect(ball.xpos, ball.ypos, ball.bwidth, ball.bheight))
+
 		if (ball.xpos <= 0):
 			ball.xspeed *= -1
 			player1.score += 1
@@ -57,12 +60,13 @@ class ball:
 		if (ball.ypos <= 0) or (ball.ypos + ball.bheight >= height):
 			ball.yspeed *= -1
 
+xspeed = random.uniform(0.5, 1)
+yspeed = 1-xspeed
+ball1 = ball(width/2, height/2, 10, 10, xspeed, yspeed)
+
 def gameLoop():
 	player1 = player("Player 1", width-20, height/2, 10, 50, 0, 0)
 	player2 = player("Player 2", 20, height/2, 10, 50, 0, 0)
-	xspeed = random.uniform(0.5, 1)
-	yspeed = 1-xspeed
-	ball1 = ball(width/2, height/2, 10, 10, xspeed, yspeed)
 	serve(ball1)
 	done = False
 	while not done:
@@ -71,13 +75,13 @@ def gameLoop():
 				done = True
 			if event.type == pygame.KEYDOWN:
 				if (event.key == pygame.K_UP):
-					player1.speed = -1
+					player1.speed = -0.5
 				if (event.key == pygame.K_DOWN):
-					player1.speed = 1
+					player1.speed = 0.5
 				if (event.key == pygame.K_w):
-					player2.speed = -1
+					player2.speed = -0.5
 				if (event.key == pygame.K_s):
-					player2.speed = 1
+					player2.speed = 0.5
 			if event.type == pygame.KEYUP:
 				if (event.key == pygame.K_UP) or (event.key == pygame.K_DOWN):
 					player1.speed = 0
@@ -85,18 +89,25 @@ def gameLoop():
 					player2.speed = 0
 
 		screen.fill(white)
-
 		player.update(player1)
 		player.update(player2)
+
 		ball.updateball(ball1, player1, player2)
+
+		text = font.render('Player 2: ' + str(player2.score) + ' / Player 1: ' + str(player1.score), True, black) 
+		textRect = text.get_rect()  
+		textRect.center = (400, 32)
+		screen.blit(text, textRect)
 
 		clock.tick(1000)
 		pygame.display.flip()
 		
 def serve(ball1):
-	xspeed = random.uniform(0.5, 1)
-	yspeed = 1-xspeed
-	ball1 = ball(width/2, height/2, 10, 10, xspeed, yspeed)
+	ball1.xspeed = random.uniform(0.5, 1.0)
+	ball1.yspeed = 1-ball1.xspeed
+	ball1.xpos = width/2
+	ball1.ypos = height/2
+	time.sleep(1)
 	return (ball1)
 
 gameLoop()
